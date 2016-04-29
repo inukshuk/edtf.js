dda1b9f4f0bc053eef334f6aa2e7501eb45f1acb
Sylvester Keil <sylvester@keil.or.at>
2016-04-30 00:42:50 +0200

    Add date year months and day matchers

--- test/support/chai.js
+++ test/support/chai.js
@@ -1,4 +1,42 @@
 'use strict'
 
+const edtf = require('../..')
 const chai = require('chai')
-global.expect = chai.expect
+
+const { expect, Assertion } = chai
+global.expect = expect
+
+chai.use(function (_, utils) {
+
+  Assertion.addProperty('date', function () {
+    expect(utils.flag(this, 'object')).to.be.instanceof(Date)
+  })
+
+  Assertion.addProperty('edtf', function () {
+    expect(utils.flag(this, 'object')).to.be.instanceof(edtf.Date)
+  })
+
+  function year(expected) {
+    const date = utils.flag(this, 'object')
+    expect(date.getFullYear()).to.eql(expected)
+  }
+
+  function months(expected) {
+    const date = utils.flag(this, 'object')
+    expect(date.getMonths()).to.eql(expected)
+  }
+
+  function days(expected) {
+    const date = utils.flag(this, 'object')
+    expect(date.getDays()).to.eql(expected)
+  }
+
+  Assertion.addChainableMethod('year', year)
+  Assertion.addChainableMethod('years', year)
+
+  Assertion.addChainableMethod('month', months)
+  Assertion.addChainableMethod('months', months)
+
+  Assertion.addChainableMethod('day', days)
+  Assertion.addChainableMethod('days', days)
+})
