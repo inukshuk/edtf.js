eed7180615741631fa7148d157e62688f48d6f02
Sylvester Keil <sylvester@keil.or.at>
2016-04-30 01:34:47 +0200

    Explicitly call Date constructor

--- src/date.js
+++ src/date.js
@@ -1,6 +1,12 @@
 'use strict'
 
 class ExtDate extends Date {
+  constructor(input) {
+    if (arguments.length !== 1) return super(...arguments)
+    if (typeof input !== 'string') return super(input)
+
+    super(input)
+  }
 }
 
 module.exports = ExtDate
