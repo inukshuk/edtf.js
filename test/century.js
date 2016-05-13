5417d83a121dbbcdfc1017e6e6d96264ce9b1347
Sylvester Keil <sylvester@keil.or.at>
2016-05-13 13:53:15 +0200

    Add Century tests

---
+++ test/century.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const { Century } = require('..')
+
+describe('Century', () => {
+
+  describe('.edtf', () => {
+    it('default', () =>
+      expect(new Century().edtf).to.match(/^\d\d$/))
+
+    it('CC', () => {
+      expect(new Century([20]).edtf).to.eql('20')
+      expect(new Century(1).edtf).to.eql('01')
+    })
+
+    it('-CC', () => {
+      expect(new Century(-9).edtf).to.eql('-09')
+      expect(new Century(-19).edtf).to.eql('-19')
+    })
+
+    it('CC~', () => {
+      expect(new Century({ values: [19], approximate: true }).edtf)
+        .to.eql('19~')
+    })
+  })
+})
