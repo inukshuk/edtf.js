9f20b8a92bee394fb795d4d1625f4f7c7af1275b
Sylvester Keil <sylvester@keil.or.at>
2016-05-13 17:56:11 +0200

    Add Interval class

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Year = require('./src/year')
 const Decade = require('./src/decade')
 const Century = require('./src/century')
 const Season = require('./src/season')
+const Interval = require('./src/interval')
 const Bitmask = require('./src/bitmask')
 const types = require('./src/types')
 
@@ -13,7 +14,11 @@ const { parse } = require('./src/parser')
 
 
 function edtf(...args) {
-  if (!args.length) return new ExtDate()
+  if (!args.length)
+    return new ExtDate()
+
+  if (args.length === 1 && typeof args[0] === 'object' && args[0].type)
+    return new edtf[args[0].type](args[0])
 
   const res = parse(...args)
   return new edtf[res.type](res)
@@ -26,6 +31,7 @@ module.exports = Object.assign(edtf, {
   Decade,
   Century,
   Season,
+  Interval,
   Bitmask,
   parse,
   sample,
---
+++ src/interval.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const assert = require('assert')
+const { parse } = require('./parser')
+
+const V = new WeakMap()
+
+
+class Interval {
+  static parse(input) {
+    return parse(input, { types: ['Interval'] })
+  }
+
+  constructor(...args) {
+    switch (args.length) {
+    case 2:
+      [this.lower, this.upper] = args
+      break
+
+    case 1:
+      switch (typeof args[0]) {
+      case 'string':
+        args[0] = Interval.parse(args[0])
+        // eslint-disable-line no-fallthrough
+
+      case 'object':
+        if (Array.isArray(args[0]))
+          args[0] = { values: args[0] }
+
+        {
+          let [obj] = args
+
+          assert(obj !== null)
+          if (obj.type) assert.equal('Interval', obj.type)
+
+          assert(obj.values)
+          assert(obj.values.length < 3)
+
+          ;[this.lower, this.upper] = obj.values
+        }
+        break
+
+      default:
+        this.lower = args[0]
+      }
+      break
+
+    case 0:
+      break
+
+    default:
+      throw new RangeError(`invalid interval value: ${args}`)
+    }
+  }
+
+  get type() {
+    return 'Interval'
+  }
+
+
+  get lower() {
+    return this.values[0]
+  }
+
+  set lower(value) {
+    this.values[0] = value // todo
+  }
+
+  get upper() {
+    return this.values[1]
+  }
+
+  set upper(value) {
+    this.values[1] = value // todo
+  }
+
+  get values() {
+    return V[this]
+  }
+
+  get earlier() {
+  }
+
+  get later() {
+  }
+
+  get edtf() {
+    return this.toEDTF()
+  }
+
+  get min() {
+    return this.lower.min
+  }
+
+  get max() {
+    return this.upper.max
+  }
+
+  toEDTF() {
+    return this.values
+      .map(v => {
+        if (!v) return ''
+        if (v === Infinity) return '*'
+        return v.edtf
+      })
+      .join('/')
+  }
+}
+
+module.exports = Interval
