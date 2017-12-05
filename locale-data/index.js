c5b069ceadab1b2e9675665c5e5475c7265dd470
Sylvester Keil <sylvester@keil.or.at>
2017-12-05 22:14:30 +0100

    Add German and French locale data

--- locale-data/index.js
+++ locale-data/index.js
@@ -2,6 +2,16 @@
 
 const locale = {}
 
-locale.en = locale['en-US'] = require('./en-US')
+locale.en = require('./en-US')
+locale.de = require('./de-DE')
+locale.fr = require('./fr-FR')
+
+alias('en', 'AU', 'CA', 'GB', 'NZ', 'SA', 'US')
+alias('de', 'AT', 'CH', 'DE')
+alias('fr', 'CH', 'FR')
+
+function alias(lc, ...args) {
+  for (const ct of args) locale[`${lc}-${ct}`] = locale[lc]
+}
 
 module.exports = locale
