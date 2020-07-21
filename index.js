f8498a89f8d5047458d34018c1813836a871bb8a
Sylvester Keil <sylvester@keil.or.at>
2020-07-21 21:53:35 +0200

    Add conditional exports for latest node.js

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-'use strict'
+import edtf from './index.cjs'
 
-const Bitmask = require('./src/bitmask')
-const types = require('./src/types')
+export const Bitmask = edtf.Bitmask
+export const defaults = edtf.defaults
+export const parse = edtf.parse
+export const sample = edtf.sample
+export const format = edtf.format
+export const types = edtf.types
 
-const { sample } = require('./src/sample')
-const { parse, defaults } = require('./src/parser')
-const { format } = require('./src/format')
-
-const { assign, keys } = Object
-
-const UNIX_TIME = /^\d{5,}$/
-
-function edtf(...args) {
-  if (!args.length)
-    return new edtf.Date()
-
-  if (args.length === 1) {
-    switch (typeof args[0]) {
-    case 'object':
-      return new (edtf[args[0].type] || edtf.Date)(args[0])
-    case 'number':
-      return new edtf.Date(args[0])
-    case 'string':
-      if ((UNIX_TIME).test(args[0]))
-        return new edtf.Date(Number(args[0]))
-    }
-  }
-
-  let res = parse(...args)
-  return new edtf[res.type](res)
-}
-
-
-module.exports = assign(edtf, types, {
-  Bitmask,
-  defaults,
-  parse,
-  sample,
-  format,
-  types: keys(types)
-})
+export default edtf
