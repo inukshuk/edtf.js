04430d54b8be7bea3ec4a60fea1d6cc0dcc7b622
Sylvester Keil <sylvester@keil.or.at>
2020-07-23 10:19:34 +0200

    Make randexp optional

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const edtf = require('./src/edtf')
 const Bitmask = require('./src/bitmask')
 const types = require('./src/types')
-const { sample } = require('./src/sample')
 const { parse, defaults } = require('./src/parser')
 const { format } = require('./src/format')
 
@@ -11,7 +10,9 @@ module.exports = Object.assign(edtf, types, {
   Bitmask,
   defaults,
   parse,
-  sample,
   format,
+  get sample() {
+    return require('./src/sample').sample
+  },
   types: Object.keys(types)
 })
