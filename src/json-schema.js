684f733056b6075a689946beb659e6da91396068
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-06-07 20:41:35 +0200

    json-schema validators

---
+++ src/json-schema.js
@@ -0,0 +1,39 @@
+// see https://rawgit.com/zaggino/z-schema/master/benchmark/results.html for a list of schema engines
+
+import { parse } from './parser.js'
+
+function isLevel(date, level, seasonIntervals) {
+  if (typeof date !== 'string') return false
+
+  try {
+    parse(date, { level, seasonIntervals })
+    return true
+  }
+  catch (err) {
+    return false
+  }
+}
+
+export const formats = {}
+for (const seasonIntervals of [ false, true ]) {
+  for (const level of [0, 1, 2]) {
+    ((level, seasonIntervals) => {
+      formats[`edtf/${level}${seasonIntervals ? '+season-intervals' : ''}`] = date => isLevel(date, { level, seasonIntervals })
+    })(level, seasonIntervals)
+}
+
+export function ajv(_ajv) {
+  for (const [format, validator] of Object.entries(formats)) {
+    _ajv.addFormat(format, validator)
+  }
+  return _ajv
+}
+
+export function zschema(_zschema) {
+  for (const [format, validator] of Object.entries(formats)) {
+    _zschema.registerFormat(format, function (date) {
+      return validator(date)
+    })
+  }
+  return _zschema
+}
