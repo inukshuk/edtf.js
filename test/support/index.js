ac7b2e744e29771882bdf3d89636dd2e1620bbc1
Sylvester Keil <sylvester@keil.or.at>
2026-03-28 17:38:21 +0100

    Lint

--- test/support/index.js
+++ test/support/index.js
@@ -1,4 +1,18 @@
-import { describe, it, before, after, beforeEach, afterEach } from 'node:test'
 import './chai.js'
+import {
+  describe,
+  it,
+  before,
+  after,
+  beforeEach,
+  afterEach
+} from 'node:test'
 
-Object.assign(globalThis, { describe, it, before, after, beforeEach, afterEach })
+Object.assign(globalThis, {
+  describe,
+  it,
+  before,
+  after,
+  beforeEach,
+  afterEach
+})
